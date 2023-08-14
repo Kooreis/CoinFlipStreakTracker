e64a0db0e14421fc5c6f6df3cd074c159010bead
Kooreis <kooreis.official@gmail.com>
2023-08-14 09:35:53 -0500

    The first update includes the import of the random module and the definition of the coin_flip function. This function simulates a coin flip by returning 'Heads' or 'Tails' based on a random integer.

--- Coin_Flip_Streak_Tracker.py
+++ Coin_Flip_Streak_Tracker.py
@@ -1,27 +1,4 @@
-```python
 import random
 
 def coin_flip():
     return 'Heads' if random.randint(0, 1) == 0 else 'Tails'
-
-def streak_tracker():
-    streak_count = 0
-    flip_result = coin_flip()
-    print(flip_result)
-
-    while True:
-        next_flip = coin_flip()
-        print(next_flip)
-
-        if next_flip == flip_result:
-            streak_count += 1
-        else:
-            streak_count = 0
-            flip_result = next_flip
-
-        if streak_count == 6:
-            print('Streak of 6!')
-            break
-
-streak_tracker()
-```
