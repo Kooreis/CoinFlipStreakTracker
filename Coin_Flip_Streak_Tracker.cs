3d62395835434a8d8748163865ac085a0c6489e4
Kooreis <kooreis.official@gmail.com>
2023-08-14 09:35:53 -0500

    Initial setup of the program. Declares the necessary variables: a Random object, an integer for the streak count, and a string to hold the result of the last coin flip.

--- Coin_Flip_Streak_Tracker.cs
+++ Coin_Flip_Streak_Tracker.cs
@@ -1,4 +1,3 @@
-```CSharp
 using System;
 
 class Program
@@ -8,31 +7,5 @@ class Program
         Random random = new Random();
         int streak = 0;
         string lastFlip = "";
-
-        while (true)
-        {
-            int flip = random.Next(0, 2);
-            string result = flip == 0 ? "Heads" : "Tails";
-
-            if (lastFlip == "")
-            {
-                lastFlip = result;
-                streak = 1;
-            }
-            else if (lastFlip == result)
-            {
-                streak++;
-            }
-            else
-            {
-                Console.WriteLine("Streak of " + lastFlip + ": " + streak);
-                lastFlip = result;
-                streak = 1;
-            }
-
-            Console.WriteLine(result);
-            System.Threading.Thread.Sleep(1000);
-        }
     }
 }
-```
