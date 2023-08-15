7e524bada6225f15d39de5bb9bfa4d2a01d371a2
Kooreis <kooreis.official@gmail.com>
2023-08-15 09:35:53 -0500

    Adds an infinite loop that simulates a coin flip. The result of the flip is stored in the 'result' variable.

--- Coin_Flip_Streak_Tracker.cs
+++ Coin_Flip_Streak_Tracker.cs
@@ -1,11 +1,5 @@
-using System;
-
-class Program
+while (true)
 {
-    static void Main(string[] args)
-    {
-        Random random = new Random();
-        int streak = 0;
-        string lastFlip = "";
-    }
+    int flip = random.Next(0, 2);
+    string result = flip == 0 ? "Heads" : "Tails";
 }
