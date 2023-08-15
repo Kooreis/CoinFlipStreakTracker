b0f6522f569f8a0f81b161807173bbbe1a90a695
Kooreis <kooreis.official@gmail.com>
2023-08-15 09:35:53 -0500

    In this update, two variables are declared and initialized. The 'streak' variable will keep track of the number of consecutive coin flips that are the same, and the 'lastFlip' variable will store the result of the last coin flip.

--- Coin_Flip_Streak_Tracker.java
+++ Coin_Flip_Streak_Tracker.java
@@ -3,5 +3,7 @@ import java.util.Random;
 public class CoinFlipStreakTracker {
     public static void main(String[] args) {
         Random rand = new Random();
+        int streak = 0;
+        String lastFlip = "";
     }
 }
